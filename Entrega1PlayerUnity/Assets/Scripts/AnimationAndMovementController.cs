cdccba68ebfe1157207f3f33b2c6f588963c3580
SweetPaco0o <daniel.sanchez.diaz@estudiantat.upc.edu>
2024-03-11 16:52:11 +0100

    Salto V1

--- Entrega1PlayerUnity/Assets/Scripts/AnimationAndMovementController.cs
+++ Entrega1PlayerUnity/Assets/Scripts/AnimationAndMovementController.cs
@@ -5,36 +5,80 @@ using UnityEngine.InputSystem;
 
 public class AnimationAndMovementController : MonoBehaviour
 {
+    //based on iHeartGameDev YT tutorials
+    //reference variables
     PlayerInput playerInput;
     CharacterController characterController;
     Animator animator;
 
+    // variables to sore optimzed parameter IDs  
     int isWalkingHash;
     int isRunningHash;
 
+    //vairables to store player movement
     Vector2 currentMovementInput;
     Vector3 currentMovement;
     Vector3 currentRunMovement;
     bool isMovementPressed;
     bool isRunPressed;
-    float rotationFactorPerFrame = 20f;
+
+    //constants
+    float rotationFactorPerFrame = 8f;
     public float Speed = 10f;
     public float WalkingSpeed = 3f;
-    public float gravity = -2f;
+
+    //gravity
+    float groundedGravity = -.05f;
+    public float gravity = -3f;
+
+    //jumping variables
+    bool isJumpPressed = false;
+    float initialJumpVelocity;
+    float maxJumpHeight = 1f;
+    float maxJumpTime = 0.5f;
+    bool isJummping = false;
+
     void Awake()
     {
+        //set reference variables
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
 
+        //set hash parameters
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
 
+        //callbacks
         playerInput.Gameplay.Move.started += onMovementInput;
         playerInput.Gameplay.Move.canceled += onMovementInput;
         playerInput.Gameplay.Move.performed += onMovementInput;
         playerInput.Gameplay.Run.started += onRun;
         playerInput.Gameplay.Run.canceled += onRun;
+        playerInput.Gameplay.Jump.started += onJump;
+        playerInput.Gameplay.Jump.canceled += onJump;
+
+        setupJumpVariables();
+    }
+    void setupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2;
+        gravity = (-2 * maxJumpHeight) / Mathf.Pow(timeToApex,2);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+    void handleJump()
+    {
+        if(!isJummping && characterController.isGrounded && isJumpPressed){
+            isJummping = true;
+            currentMovement.y = initialJumpVelocity * .5f;
+            currentRunMovement.y = initialJumpVelocity * .5f;
+        } else if (!isJumpPressed && isJummping && characterController.isGrounded){
+            isJummping = false;
+        }
+    }
+    void onJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
     }
     void onRun (InputAction.CallbackContext context){
         isRunPressed = context.ReadValueAsButton();
@@ -86,17 +130,18 @@ public class AnimationAndMovementController : MonoBehaviour
     void handleGravity()
     {
         if(characterController.isGrounded){
-            float groundedGravity = -.05f;
             currentMovement.y = groundedGravity;
             currentRunMovement.y = groundedGravity;
         } else{
-            currentMovement.y += gravity;
-            currentRunMovement.y += gravity;
+            float previousYVelocity = currentMovement.y;
+            float newYVelocity = currentMovement.y + (gravity * Time.deltaTime);
+            float nextYVelocity = (previousYVelocity + newYVelocity) * .5f;
+            currentMovement.y = nextYVelocity;
+            currentRunMovement.y = nextYVelocity;
         }
     }
     void Update()
     {
-        handleGravity();
         handleRotation();
         handleAnimation();
 
@@ -105,6 +150,9 @@ public class AnimationAndMovementController : MonoBehaviour
         } else{
             characterController.Move(currentMovement * Time.deltaTime);
         }
+
+        handleGravity();
+        handleJump();
     }
     void OnEnable()
     {
