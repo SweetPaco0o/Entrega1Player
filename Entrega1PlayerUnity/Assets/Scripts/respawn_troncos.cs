33ffd339f1755f72d0a95d0cf76c8848350aee4a
SergiSabrosuras3000 <sergi.martinez.somolinos@estudiantat.upc.edu>
2024-03-23 10:55:16 +0100

    suelito bacano

---
+++ Entrega1PlayerUnity/Assets/Scripts/respawn_troncos.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn_troncos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(4.22f, 14.63957f, -38.2f); 
+        transform.position = new Vector3(0.87f, 16.5f, 52f);
+    }
+}
