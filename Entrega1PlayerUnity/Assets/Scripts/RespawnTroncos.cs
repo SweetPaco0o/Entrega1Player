901da9ced43c8f749964d3d1c6a1d26df65eb6af
SweetPaco0o <daniel.sanchez.diaz@estudiantat.upc.edu>
2024-03-24 00:10:41 +0100

    Revert "Merge branch 'main' of https://github.com/SweetPaco0o/Entrega1Player"
    This reverts commit 84db295ddf1dddacb4f4080095dc15db4f5e8ab0, reversing
    changes made to 50eb1abac45cb73b49ceac744c8566edd853818a.

--- Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
+++ Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
@@ -8,33 +8,10 @@ public class RespawnTroncos : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-    // Check if the collided object has the "Player" tag
-    if (collision.gameObject.CompareTag("Player"))
-    {
-        Debug.Log("Collision with Player detected");
-        
-
-        // Get the Rigidbody component of the collided object
-        Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-            
-
-        // Check if the Rigidbody component is not null
-        if (playerRigidbody != null)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            // Perform actions using playerRigidbody
-            Debug.Log("Player Rigidbody found");
-                die.Death();
-
-        }
-        else
-        {
-            Debug.LogWarning("Player Rigidbody not found.");
+            Debug.Log("RespawnTroncos");
+            transform.position = respawnPosition1;
         }
     }
-    else
-    {
-        Debug.LogWarning("Collision with non-Player object");
-    }
-}
-
 }
