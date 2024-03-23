c8b812110a9a4b17bf6a97c2ff3431b9f8a0bbe6
SergiSabrosuras3000 <sergi.martinez.somolinos@estudiantat.upc.edu>
2024-03-23 12:37:07 +0100

    mejora tronco

--- Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
+++ Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class RespawnTroncos : MonoBehaviour
 {
-    public Transform spawnPoint; 
+    public Vector3 respawnPosition1 = new Vector3(4.22f, 14.63957f, -38.2f); 
+    public Vector3 respawnPosition2 = new Vector3(0.87f, 16.5f, 52f); 
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player")) 
+        if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Colisión detectada");
             
-            other.transform.position = spawnPoint.position;
+          
+            Vector3 respawnPosition = Random.Range(0, 2) == 0 ? respawnPosition1 : respawnPosition2;
+            Debug.Log("Respawn en posición: " + respawnPosition); 
+
+            
+            collision.gameObject.transform.position = respawnPosition;
         }
     }
 }
-
