ee23cde7e6602b1855c513d000bbe8183af17c4a
bielelestudiante <biel.serrallonga@estudiantat.upc.edu>
2024-03-23 14:22:43 +0100

    Troncos gilipollas

--- Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
+++ Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 
 public class RespawnTroncos : MonoBehaviour
 {
-    public Vector3 respawnPosition1 = new Vector3(4.22f, 14.63957f, -38.2f); 
-    public Vector3 respawnPosition2 = new Vector3(0.87f, 16.5f, 52f); 
+    public Vector3 respawnPosition1 = new Vector3(4.22f, 14.63957f, -38.2f);
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Colisión detectada");
-            
-          
-            Vector3 respawnPosition = Random.Range(0, 2) == 0 ? respawnPosition1 : respawnPosition2;
-            Debug.Log("Respawn en posición: " + respawnPosition); 
-
-            
-            collision.gameObject.transform.position = respawnPosition;
+            Debug.Log("RespawnTroncos");
+            transform.position = respawnPosition1;
         }
     }
 }
