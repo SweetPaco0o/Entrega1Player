92611f163471042b3c30c91de1b31592883ffe44
SweetPaco0o <daniel.sanchez.diaz@estudiantat.upc.edu>
2024-03-24 01:01:02 +0100

    AHORA?

--- Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
+++ Entrega1PlayerUnity/Assets/Scripts/RespawnTroncos.cs
@@ -5,13 +5,42 @@ using UnityEngine;
 public class RespawnTroncos : MonoBehaviour
 {
     public Vector3 respawnPosition1 = new Vector3(4.22f, 14.63957f, -38.2f);
+    private Die die;
+
+    private void Start()
+    {
+        die = GetComponent<Die>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+    // Check if the collided object has the "Player" tag
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        Debug.Log("Collision with Player detected");
+        
+
+        // Get the Rigidbody component of the collided object
+        Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            
+
+        // Check if the Rigidbody component is not null
+        if (playerRigidbody != null)
         {
-            Debug.Log("RespawnTroncos");
-            transform.position = respawnPosition1;
+            // Perform actions using playerRigidbody
+            Debug.Log("Player Rigidbody found");
+                die.Death();
+
+        }
+        else
+        {
+            Debug.LogWarning("Player Rigidbody not found.");
         }
     }
+    else
+    {
+       // Debug.LogWarning("Collision with non-Player object");
+    }
+}
+
 }
